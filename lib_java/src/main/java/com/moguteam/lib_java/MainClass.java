6caeab0b14bce0d91c63eadf4c3f6865e999b364
luoChen <hank.luo@xianghuanji.com>
2020-08-07 10:38:00 +0800

    添加 java object layout库 可以打印 对象占用情况

--- lib_java/src/main/java/com/moguteam/lib_java/MainClass.java
+++ lib_java/src/main/java/com/moguteam/lib_java/MainClass.java
@@ -1,16 +1,23 @@
 package com.moguteam.lib_java;
 
 
+import org.openjdk.jol.info.ClassLayout;
+
 import java.util.ArrayList;
 
 public class MainClass {
 
+    static Integer i = 1;
+    static Boolean b = false;
 
     public static void main(String[] args) {
+        printLayout(i);
+        printLayout(b);
 
-        ArrayList<TClass> list = new ArrayList<>();
-        list.add(new TClass());
+    }
 
+    private static void printLayout(Object o) {
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
     }
 
 }
